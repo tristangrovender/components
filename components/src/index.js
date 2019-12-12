85b8577a4e00a2ba76755f86616623635a9e282c
Tristan Grovender <therealtristangrovender@gmail.com>
2019-12-12 12:23:47 -0600

    refactored blog posts using props

--- components/src/index.js
+++ components/src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from 'faker';
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail
+                author="Tristan"
+                timeAgo="Today at 4:45PM"
+                blogContent="Nice blog post!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Joshua"
+                timeAgo="Today at 6:45PM"
+                blogContent="I can make one too!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Kia"
+                timeAgo="Today at 3:15PM"
+                blogContent="Wow this is fun!"
+                avatar={faker.image.avatar()}
+            />
         </div>
     );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
